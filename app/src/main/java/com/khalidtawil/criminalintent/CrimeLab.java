f57864e3e2d0a09eba89a8f4c4f9c85d9efed673
Khalid Tawil <tawilk2014@gmail.com>
2015-07-18 16:01:40 -0400

    Created a ListView

---
+++ app/src/main/java/com/khalidtawil/criminalintent/CrimeLab.java
@@ -0,0 +1,49 @@
+package com.khalidtawil.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by khalidtawil on 7/13/15.
+ * This class is a singleton representing a list of crimes
+ */
+public class CrimeLab {
+    private ArrayList<Crime> mCrimes;
+
+    private static CrimeLab sCrimeLab;
+    private Context mAppContext;
+
+    private CrimeLab(Context appContext){
+        mAppContext = appContext;
+        mCrimes = new ArrayList<Crime>();
+        // Populates the arraylist with 100 crimes
+        for(int i = 0; i < 99; i++){
+            Crime c = new Crime();
+            c.setTitle("Crime #"+i);
+            c.setSolved(i%2==0);
+            mCrimes.add(c);
+        }
+    }
+
+    public static CrimeLab get(Context c){
+        if (sCrimeLab == null){
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+        return sCrimeLab;
+    }
+
+    public ArrayList<Crime> getCrimes(){
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for (Crime c : mCrimes){
+            if (c.getID().equals(id))
+                return c;
+        }
+        return null;
+    }
+
+}
